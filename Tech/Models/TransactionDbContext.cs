8c78e0bdf78fdcca5af5ecd139cd22970bb3b1c1
Muhammad Mubashir <57788343+engrmalikmubashir@users.noreply.github.com>
2020-04-07 19:47:04 +0500

    Add project files.

---
+++ Tech/Models/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tech.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
